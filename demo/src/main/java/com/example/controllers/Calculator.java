22cd48e8d636cfb26eb780ba6b90d4d06585cf8f
BaZZcheesy <107484617+BaZZcheesy@users.noreply.github.com>
2024-04-17 00:12:15 +0200

    Prepared for demo

--- demo/src/main/java/com/example/controllers/Calculator.java
+++ demo/src/main/java/com/example/controllers/Calculator.java
@@ -1,8 +1,19 @@
 package com.example.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping("/api")
 public class Calculator {
     
+    @CrossOrigin
+    @GetMapping( value = "/")
+    public String getIndex() {
+        return "<h1>Hello World!</h1>";
+    }
 }
--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,51 +1,118 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 
-class App extends React.Component {
-  
-  constructor(props) {
-    super(props);
-    this.state = {
-      num1 : "",
-      num2 : ""
-    };
+function App() {
+  const [responseOut, setResponseOut] = useState("");
+
+  const handleSubmitV1 = event => {
+    event.preventDefault();
+    fetch("http://localhost:8080/api/hello", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "API-VERSION": "1"
+      },
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      return response.text();
+    })
+    .then(data => {
+      setResponseOut(data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      setResponseOut("Error fetching data");
+    });
   }
 
-  handleChangeNum1 = event => {
-    this.setState({ num1: event.target.value });
+  const handleSubmitV2 = event => {
+    event.preventDefault();
+    fetch("http://localhost:8080/api/hello", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "API-VERSION": "2"
+      },
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      return response.text();
+    })
+    .then(data => {
+      setResponseOut(data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      setResponseOut("Error fetching data");
+    });
   }
 
-  handleChangeNum2 = event => {
-    this.setState({ num2: event.target.value });
+  const handleSubmitV3 = event => {
+    event.preventDefault();
+    fetch("http://localhost:8080/api/v1/hello")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      return response.text();
+    })
+    .then(data => {
+      setResponseOut(data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      setResponseOut("Error fetching data");
+    });
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-
-          <form onSubmit="">
-            <input type="number" value={this.state.num1} onChange={this.handleChangeNum1} />
-            <input type="number" value={this.state.num2} onChange={this.handleChangeNum2} />
-            <button type="submit">Submit</button>
-          </form>
-        </header>
-      </div>
-    )
+  const handleSubmitV4 = event => {
+    event.preventDefault();
+    fetch("http://localhost:8080/api/v2/hello")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      return response.text();
+    })
+    .then(data => {
+      setResponseOut(data);
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+      setResponseOut("Error fetching data");
+    });
   }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+          <p>{responseOut}</p>
+          <button onClick={handleSubmitV1}>Submit</button>
+          <button onClick={handleSubmitV2}>Submit</button>
+          <button onClick={handleSubmitV3}>Submit</button>
+          <button onClick={handleSubmitV4}>Submit</button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
